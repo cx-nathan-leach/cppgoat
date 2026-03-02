269c1db0833d7fb12e66191b59aa27564db38fb2
Nathan Leach <203706735+cx-nathan-leach@users.noreply.github.com>
2026-03-02 18:53:48 +0000

    fix db create

--- DAL/sqlite/sqlitedal.cc
+++ DAL/sqlite/sqlitedal.cc
@@ -1,13 +1,20 @@
 #include "sqlitedal.hpp"
-#include "sqlite3.h"
 
 
 namespace cppgoat::DAL
 {
 
-    SqliteDAL::SqliteDAL()
+    SqliteDAL::SqliteDAL() : _db(NULL)
     {
-      
+      auto result = sqlite3_open_v2("users.db", &_db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE | SQLITE_OPEN_FULLMUTEX, NULL);
+
+      auto x = result == SQLITE_OK;
+      int y = 0;
+    }
+
+    SqliteDAL::~SqliteDAL()
+    {
+      sqlite3_close_v2(_db);
     }
 
     bool SqliteDAL::ValidateLogin(const std::string& username, const std::string& password)
--- DAL/sqlite/sqlitedal.hpp
+++ DAL/sqlite/sqlitedal.hpp
@@ -1,17 +1,20 @@
 #ifndef __SQLITEDAL_HPP__
 #define __SQLITEDAL_HPP__
 #include "user_dal.hpp"
+#include "sqlite3.h"
 
 namespace cppgoat::DAL
 {
   class SqliteDAL : public cppgoat::DAL::IUserAccountDAL
   {
+    private:
+      sqlite3 *_db;
     public:
     SqliteDAL();
 
     virtual bool ValidateLogin(const std::string& username, const std::string& password);
     virtual bool CreateUser(const std::string& username, const std::string& password);
-    virtual ~SqliteDAL(){}
+    virtual ~SqliteDAL();
 
   };
 
